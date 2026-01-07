d396b307a4238d54f13ecafc40e5683b454ee31d
Mario Lazzari <mario.lazzari@gmail.com>
2026-01-07 21:13:58 +0100

    fix: scroll on mobile

--- components/Buttons/SaveButton.tsx
+++ components/Buttons/SaveButton.tsx
@@ -3,21 +3,18 @@ import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { RefreshCcw, Save } from "lucide-react";
 import { ComponentProps } from "react";
-import { DialogClose } from "@radix-ui/react-dialog";
 
 export function SaveButton(props: ComponentProps<"button">) {
   const { pending } = useFormStatus();
 
   return (
-    <DialogClose>
-      <Button
-        className="w-28 cursor-pointer"
-        type="submit"
-        disabled={pending}
-        {...props}
-      >
-        {pending ? <RefreshCcw className="animate-spin" /> : <Save />} Salva
-      </Button>
-    </DialogClose>
+    <Button
+      className="w-28 cursor-pointer"
+      type="submit"
+      disabled={pending}
+      {...props}
+    >
+      {pending ? <RefreshCcw className="animate-spin" /> : <Save />} Salva
+    </Button>
   );
 }
